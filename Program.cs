f92b6a4b866111b871cea79646f8a1631b34c1e0
Zach Stein <z.b.stein@gmail.com>
2019-10-24 15:00:17 -0700

    vs code project initialization (dotnet new console; dotnet run) and gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace swiftly_services_exercse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
