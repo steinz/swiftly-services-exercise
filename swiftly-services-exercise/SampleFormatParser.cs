702eef025c5e7516ca85f096009355b0cacac55a
Zach Stein <z.b.stein@gmail.com>
2019-11-13 17:23:50 -0800

    Finish parser (including helper methods and provided unit tests)

--- swiftly-services-exercise/SampleFormatParser.cs
+++ swiftly-services-exercise/SampleFormatParser.cs
@@ -1,36 +1,72 @@
 namespace swiftly_services_exercise
 {
+    // TODO: Parser interface
+
+    // Parses the sample format described in the spec at
+    // https://github.com/Swiftly-Systems/code-exercise-services/blob/master/ProductInformationIntegrationSpec.md
     public class SampleFormatParser
     {
+        // TODO: Replace magic numbers with statics.
+
+        /// The string currently being parsed.
+        private string m_s;
+
+        // TODO: Consider other currency types.
+        decimal ParseCurrency(int i, int l) {
+            return decimal.Parse(m_s.Substring(i, l)) / 100m;
+        }
+
+        string FormatCurrencyForDisplay(decimal x) {
+            // TODO: Consider culture specific formatting.
+            // TODO: Commas
+            // TODO: Use decimal.ToString("C"); in a real implementation.
+            var rounded = decimal.Round(x, 2, System.MidpointRounding.ToZero);
+            var fixedWidth = rounded.ToString("N2");
+            return $"${fixedWidth}";
+        }
+
+        decimal CalculatorPrice(int singularPriceIndex, int splitPriceIndex, int forXIndex) {
+            decimal singularPrice = ParseCurrency(singularPriceIndex, 8);
+            decimal calculatorPrice;
+            if (singularPrice != 0) {
+                calculatorPrice = singularPrice;
+            } else {
+                decimal splitPrice = ParseCurrency(splitPriceIndex, 8);
+                int forX = int.Parse(m_s.Substring(forXIndex, 8));
+                calculatorPrice = splitPrice == 0 ? 0 : splitPrice / forX;
+            }
+            return decimal.Round(calculatorPrice, 4, System.MidpointRounding.ToZero);
+        }
+
+        bool CheckFlag(int i) {
+            // TODO: Check i is in range.
+            // TODO: Assert 'N' if not 'Y'
+            int j = 123 + i - 1;
+            char c = m_s[j];
+            return c == 'Y';
+        }
+
         public ProductRecord Parse(string s) {
-            ProductRecord result = new ProductRecord();
+            m_s = s;
+
+            var result = new ProductRecord();
 
             result.ProductID = int.Parse(s.Substring(0, 8));
             result.ProductDescription = s.Substring(9, 59).Trim();
             
-            // TODO: ParseCurrency helper
-            decimal regularSingularPrice = decimal.Parse(s.Substring(69, 8)) / 100m;
-            if (regularSingularPrice == 0) {
-                decimal regularSplitPrice = decimal.Parse(s.Substring(87, 8)) / 100m;
-                int regularForX = int.Parse(s.Substring(106, 8));
-                result.RegularCalculatorPrice = regularSplitPrice / regularForX;
-            } else {
-                result.RegularCalculatorPrice = regularSingularPrice;
-            }
-            result.RegularCalculatorPrice = decimal.Round(result.RegularCalculatorPrice, 4,
-                System.MidpointRounding.ToZero);
-            // TODO: Add $ and commas here
-            result.RegularDisplayPrice = decimal.Round(result.RegularCalculatorPrice, 2,
-                System.MidpointRounding.ToZero).ToString();
+            // TODO: 106 off by one?
+            result.RegularCalculatorPrice = CalculatorPrice(69, 87, 105);
+            result.RegularDisplayPrice = FormatCurrencyForDisplay(result.RegularCalculatorPrice);
 
-            // TODO
-            //result.PromotionalDisplayPrice = 
-            //result.PromotionalCalculatorPrice = 
+            result.PromotionalCalculatorPrice = CalculatorPrice(78, 96, 114);
+            result.PromotionalDisplayPrice = result.PromotionalCalculatorPrice == 0 ?
+                "N/A" :  FormatCurrencyForDisplay(result.PromotionalCalculatorPrice);
 
-            //result.TaxRate = 
-            //result.Measure = 
-            //result.ProduceSize = 
+            result.TaxRate = CheckFlag(5) ? 7.775m : 0;
+            result.Measure = CheckFlag(3) ? UnitOfMeasure.Pound : UnitOfMeasure.Each;
+            result.ProductSize = s.Substring(133, 9).Trim();
 
+            m_s = null;
             return result;
         }
     }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -6,6 +6,8 @@ namespace test
 {
     public class Tests
     {
+        private const decimal TaxRate = 7.775m;
+
         [SetUp]
         public void Setup()
         {
@@ -22,11 +24,79 @@ namespace test
             Assert.That(product.ProductDescription, Is.EqualTo("Kimchi-flavored white rice"));
             Assert.That(product.RegularCalculatorPrice, Is.EqualTo(5.67m));
             // TODO: Revisit output formatting
-            Assert.That(product.RegularDisplayPrice, Is.EqualTo("5.67"));
+            Assert.That(product.RegularDisplayPrice, Is.EqualTo("$5.67"));
+            Assert.That(product.PromotionalCalculatorPrice, Is.EqualTo(0m));
+            Assert.That(product.PromotionalDisplayPrice, Is.EqualTo("N/A"));
             
-            // TODO
-            //Assert.That(product.ProductSize, Is.EqualTo("18oz"));
+            Assert.That(product.TaxRate, Is.EqualTo(0m));
+            Assert.That(product.Measure, Is.EqualTo(UnitOfMeasure.Each));
+
+            Assert.That(product.ProductSize, Is.EqualTo("18oz"));
         }
+
+        [Test]
+        public void Test2()
+        {
+            SampleFormatParser parser = new SampleFormatParser();
+            ProductRecord product = parser.Parse(
+                "14963801 Generic Soda 12-pack                                        00000000 00000549 00001300 00000000 00000002 00000000 NNNNYNNNN   12x12oz");
+
+            Assert.That(product.ProductID, Is.EqualTo(14963801));
+            Assert.That(product.ProductDescription, Is.EqualTo("Generic Soda 12-pack"));
+            Assert.That(product.RegularCalculatorPrice, Is.EqualTo(6.50m));
+            // TODO: Revisit output formatting
+            Assert.That(product.RegularDisplayPrice, Is.EqualTo("$6.50"));
+            Assert.That(product.PromotionalCalculatorPrice, Is.EqualTo(5.49m));
+            Assert.That(product.PromotionalDisplayPrice, Is.EqualTo("$5.49"));
+
+            Assert.That(product.TaxRate, Is.EqualTo(TaxRate));
+            Assert.That(product.Measure, Is.EqualTo(UnitOfMeasure.Each));
+
+            Assert.That(product.ProductSize, Is.EqualTo("12x12oz"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            SampleFormatParser parser = new SampleFormatParser();
+            ProductRecord product = parser.Parse(
+                "40123401 Marlboro Cigarettes                                         00001000 00000549 00000000 00000000 00000000 00000000 YNNNNNNNN          ");
+
+            Assert.That(product.ProductID, Is.EqualTo(40123401));
+            Assert.That(product.ProductDescription, Is.EqualTo("Marlboro Cigarettes"));
+            Assert.That(product.RegularCalculatorPrice, Is.EqualTo(10.00m));
+            // TODO: Revisit output formatting
+            Assert.That(product.RegularDisplayPrice, Is.EqualTo("$10.00"));
+            Assert.That(product.PromotionalCalculatorPrice, Is.EqualTo(5.49m));
+            Assert.That(product.PromotionalDisplayPrice, Is.EqualTo("$5.49"));
+
+            Assert.That(product.TaxRate, Is.EqualTo(0.0m));
+            Assert.That(product.Measure, Is.EqualTo(UnitOfMeasure.Each));
+
+            Assert.That(product.ProductSize, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            SampleFormatParser parser = new SampleFormatParser();
+            ProductRecord product = parser.Parse(
+                "50133333 Fuji Apples (Organic)                                       00000349 00000000 00000000 00000000 00000000 00000000 NNYNNNNNN        lb");
+
+            Assert.That(product.ProductID, Is.EqualTo(50133333));
+            Assert.That(product.ProductDescription, Is.EqualTo("Fuji Apples (Organic)"));
+            Assert.That(product.RegularCalculatorPrice, Is.EqualTo(3.49m));
+            // TODO: Revisit output formatting
+            Assert.That(product.RegularDisplayPrice, Is.EqualTo("$3.49"));
+            Assert.That(product.PromotionalCalculatorPrice, Is.EqualTo(0.0m));
+            Assert.That(product.PromotionalDisplayPrice, Is.EqualTo("N/A"));
+
+            Assert.That(product.TaxRate, Is.EqualTo(0.0m));
+            Assert.That(product.Measure, Is.EqualTo(UnitOfMeasure.Pound));
+
+            Assert.That(product.ProductSize, Is.EqualTo("lb"));
+        }
+
 
         [Test]
         public void TestParsingBoundaries_SinglePrice()
@@ -36,10 +106,13 @@ namespace test
                 "555555556aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab555555556444444446000000006000000006000000006000000006NNNNNNNNNYaaaaaaaaa");
 
             // TODO: Test against SampleProductFormat instead of ProductRecord class/interface
+            // so that we can check ForX values.
             Assert.That(product.ProductID, Is.EqualTo(55555555));
             Assert.That(product.ProductDescription, Is.EqualTo(new String('a', 59)));
             Assert.That(product.RegularCalculatorPrice, Is.EqualTo(555555.55m));
-            // TODO: Other fields
+            Assert.That(product.PromotionalCalculatorPrice, Is.EqualTo(444444.44m));
+            // TODO: Flags
+            Assert.That(product.ProductSize, Is.EqualTo(new String('a', 9)));
         }
     }
 }
